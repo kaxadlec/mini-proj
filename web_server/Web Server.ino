19d6c8a3efbea18334690593db659095ac1f2222
kaxadlec <kaxadlec@gmail.com>
2023-07-19 16:29:29 +0900

    webserver v0.2 (+ OLED)

--- web_server/Web Server.ino
+++ web_server/Web Server.ino
@@ -1,16 +1,18 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <WebServer.h>
+#include "oled_u8g2.h"  // OLED Display
 
 const char* ssid = "SmartFactory";               // WIFI ID
 const char* password = "inha4885";               // WIFI PW
 
-// create WebServer object, port
-WebServer server(80);                
+OLED_U8G2 oled;   // create OLED object
+WebServer server(80);              // create WebServer object, port 
 
 
 void setup(void) {
   Serial.begin(115200);   // ESP32 baud rate 
+  oled.setup();
   WiFi.mode(WIFI_STA);    // Set connection mode
   WiFi.begin(ssid, password);  // try to access WIFI
   Serial.println("");
@@ -33,15 +35,19 @@ void setup(void) {
 
 
 void loop(void) {
+  oled.setLine(1, "inha univ");          // OLED 첫 번째 줄 : 시스템 이름
+  oled.setLine(2, "web server");                     // OLED 두 번째 줄 : count 값
+  oled.setLine(3, "---------------------");   // OLED 세 번째 줄 
+  oled.display();
+
   server.handleClient();   // clients process
   delay(2);
 }
+
 
 void handleRootEvent() {           // function to access through root                   
   Serial.println("main page");     // info to access page with serial            
   server.send(200, "text/plain", "It's SmartFactory WEBSERVER"); // status code 200(ok) , format, message
 }
-
-
 
 
